df3e9c965c3cb2c4f4d5e3c6b0f8e9f76ecdaad6
Inderpreet <inder.preet15500@gmail.com>
2024-04-22 22:45:51 +0100

    my first java program

---
+++ MyFirstClass.java
@@ -0,0 +1,47 @@
+package MyFirstPackage;
+
+public class MyFirstClass {
+    public static void main(String[] args) {
+        System.out.println("What is software");
+        System.out.println("What are software risks");
+        System.out.println("What is software testing");
+        System.out.println("Benefits of sofware testing");
+        System.out.println("What is manual testing");
+        System.out.println("What is software development life cycle");
+        System.out.println("What is software testing life cycle");
+        System.out.println("What are challenges of manual testing");
+        System.out.println("What are principles of manual testing");
+        System.out.println("Test plan and its benefits");
+        System.out.println("Test plan template");
+        System.out.println("Test scenario and template");
+        System.out.println("Test case and Template");
+        System.out.println("Test scenario vs test case");
+        System.out.println("Testin techniques");
+        System.out.println("What is a bug");
+        System.out.println("Bug lifecycle");
+        System.out.println("Bug template");
+        System.out.println("Severity vs Priority");
+        System.out.println("Testing levels");
+        System.out.println("White box testing");
+        System.out.println("Black box testing");
+        System.out.println("unit testing");
+        System.out.println("Integration testing");
+        System.out.println("Functional testing");
+        System.out.println("Non functional testing");
+        System.out.println("performance testing");
+        System.out.println("UAT testing");
+        System.out.println("Static Testing");
+        System.out.println("Dynamic Testing");
+        System.out.println("Exploratory Testing");
+        System.out.println("Ad-Hoc Testing");
+        System.out.println("Software Development Models");
+        System.out.println("Waterfall model");
+        System.out.println("Iterative model");
+        System.out.println("V-model");
+        System.out.println("Verification and Validation");
+        System.out.println("Agile");
+        System.out.println("Done and Ready");
+
+
+    }
+}
